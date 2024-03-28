58c4a70679617449fbc00aa7c2c273c8487bcf31
wissam alshohof <wissam.alshohof@gmail.com>
2024-03-28 04:32:41 +0400

    initial template color picker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, model } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
+
+type Color = any;
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'wissam-alshohof.github.io';
+  color = model<Color>();
+
+  setColor(e:any) {
+    console.log(e);
+    this.color.set(e.target.value as string);
+  document.body.setAttribute('style',`--bgClr: ${this.color()};`)
+  }
 }
